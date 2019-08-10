def96a166c915a5ac4cdd6daa8a9fc44ee32795a
neirautomatae <neirautomatae@gmail.com>
2019-08-10 17:47:29 +0700

    update

--- app/src/main/java/com/example/hotelhealthy2/Thegrid.java
+++ app/src/main/java/com/example/hotelhealthy2/Thegrid.java
@@ -9,9 +9,9 @@ import android.widget.Button;
 
 public class Thegrid extends AppCompatActivity {
 
-    private Button nextfood1;
     private Button nextfood2;
     private Button nextfood3;
+    private Button nextfood4;
     private Button backthe01;
     private Button bkhomethe01;
 
@@ -19,25 +19,17 @@ public class Thegrid extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thegrid);
-        nextfood1 = findViewById(R.id.btnfood1);
         nextfood2 = findViewById(R.id.btnfood2);
         nextfood3 = findViewById(R.id.btnfood3);
+        nextfood4 = findViewById(R.id.btnfood4);
         backthe01 = findViewById(R.id.btnbackthe);
         bkhomethe01 = findViewById(R.id.btnhomethe);
-
-        nextfood1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent gofood1 = new Intent(Thegrid.this , Food.class);
-                startActivity(gofood1);
-            }
-        });
 
 
         nextfood2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent gofood2 = new Intent(Thegrid.this , Food2.class);
+                Intent gofood2 = new Intent(Thegrid.this , Food.class);
                 startActivity(gofood2);
             }
         });
@@ -45,11 +37,18 @@ public class Thegrid extends AppCompatActivity {
         nextfood3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent gofood3 = new Intent(Thegrid.this , Food3.class);
+                Intent gofood3 = new Intent(Thegrid.this , Food2.class);
                 startActivity(gofood3);
             }
         });
 
+        nextfood4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gofood3 = new Intent(Thegrid.this , Food3.class);
+                startActivity(gofood3);
+            }
+        });
 
         backthe01.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,7 +60,7 @@ public class Thegrid extends AppCompatActivity {
         bkhomethe01.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent nexthomefod01 = new Intent(Thegrid.this , Nutrition.class);
+                Intent nexthomefod01 = new Intent(Thegrid.this , MainActivity.class);
                 startActivity(nexthomefod01);
             }
         });
